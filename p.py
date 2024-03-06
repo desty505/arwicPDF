f669f920775265180125214710c466652434ee37
desty505 <dogmai@outlook.jp>
2024-03-06 19:41:50 +0900

    修正

--- p.py
+++
@@ -1,8 +0,0 @@
-import pkg_resources
-
-# インストールされている全てのパッケージを取得
-installed_packages = pkg_resources.working_set
-
-print("Installed packages:")
-for package in installed_packages:
-    print(f"{package.key}=={package.version}")
